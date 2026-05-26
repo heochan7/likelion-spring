899237397893d412ca4bf6299e32b2217e62606d
Heochan1125 <110871957+heochan7@users.noreply.github.com>
2026-05-26 11:55:10 +0900

    mission6

--- src/main/java/org/example/likelionspring/LikelionSpringApplication.java
+++ src/main/java/org/example/likelionspring/LikelionSpringApplication.java
@@ -9,10 +9,8 @@ import org.springframework.context.ApplicationContext;
 public class LikelionSpringApplication {
 
     public static void main(String[] args) {
-        // run 메서드의 반환값을 받아 컨텍스트를 저장합니다.
         ApplicationContext context = SpringApplication.run(LikelionSpringApplication.class, args);
 
-        // 생성된 context 인스턴스를 통해 빈을 가져옵니다.
         MemberService memberService = context.getBean(MemberService.class);
     }
 }
