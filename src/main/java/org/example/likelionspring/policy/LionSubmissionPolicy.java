a233f4515da3c9667ef81585284d15e64f5d157c
Heochan1125 <110871957+heochan7@users.noreply.github.com>
2026-05-26 11:44:03 +0900

    mission6

---
+++ src/main/java/org/example/likelionspring/policy/LionSubmissionPolicy.java
@@ -0,0 +1,9 @@
+package org.example.likelionspring.policy;
+
+public class LionSubmissionPolicy implements SubmissionPolicy {
+
+    @Override
+    public boolean canSubmit() {
+        return true;
+    }
+}
